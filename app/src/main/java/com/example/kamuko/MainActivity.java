1edad5b2a5d5feb6e6af95977c48b890e8eca379
Gavsh96 <78014354+Gavsh96@users.noreply.github.com>
2022-09-06 19:52:33 +0530

    Creating DB Test

--- app/src/main/java/com/example/kamuko/MainActivity.java
+++ app/src/main/java/com/example/kamuko/MainActivity.java
@@ -12,10 +12,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RestaurantDBModel rDBm = new RestaurantDBModel();
+
         FragmentManager frag = getSupportFragmentManager();
         SpecialMenuFragment specialMenuFragment = (SpecialMenuFragment) frag.findFragmentById(R.id.frameLayout);
-
+        CreateRestaurants(rDBm);
         specialMenuFragment = new SpecialMenuFragment();
         frag.beginTransaction().add(R.id.frameLayout, specialMenuFragment).commit();
+    }
+
+    private void CreateRestaurants(RestaurantDBModel rDBm)
+    {
+        Restaurant r1 = new Restaurant("1234", "Shamika's Chicken Shop", "kukula");
+        Restaurant r2 = new Restaurant("1235", "Akbho Bell", "taco");
+        Restaurant r3 = new Restaurant("1236", "Gav's Pizza", "pizza");
+        Restaurant r4 = new Restaurant("1237", "Hindiye kade", "roti");
+        Restaurant r5 = new Restaurant("1238", "Beepan plaintea", "tea");
+
+        rDBm.addRestaurant(r1);
+        rDBm.addRestaurant(r2);
+        rDBm.addRestaurant(r3);
+        rDBm.addRestaurant(r4);
+        rDBm.addRestaurant(r5);
     }
 }
