c62d6bb973ec60ae786e87de8dc601a289b4e205
Gavsh96 <78014354+Gavsh96@users.noreply.github.com>
2022-09-13 19:33:18 +0530

    Added New Restaurants

--- app/src/main/java/com/example/kamuko/MainActivity.java
+++ app/src/main/java/com/example/kamuko/MainActivity.java
@@ -56,16 +56,36 @@ public class MainActivity extends AppCompatActivity {
 
     private void CreateRestaurants(RestaurantDBModel rDBm)
     {
+        //Creating Restaurants
         Restaurant r1 = new Restaurant("1234", "Shamika's Chicken Shop", R.drawable.restaurant);
-        Restaurant r2 = new Restaurant("1235", "Akbho Bell", R.drawable.restaurant);
+
+        Restaurant r2 = new Restaurant("1235", "Posh By Akbho", R.drawable.restaurant);
+        Menu r2m1 = new Menu("01", "Posh Fries", R.drawable.profile, r2.getId());
+        Menu r2m2 = new Menu("02", "Chicken Posh Burger", R.drawable.profile, r2.getId());
+        Menu r2m3 = new Menu("03", "Spicy Beef Burger", R.drawable.profile, r2.getId());
+        Menu r2m4 = new Menu("04", "Nai Miris Pork Posh Burger", R.drawable.profile, r2.getId());
+        Menu r2m5 = new Menu("05", "Gon Wada Special", R.drawable.profile, r2.getId());
+
         Restaurant r3 = new Restaurant("1236", "Gav's Pizza", R.drawable.restaurant);
+        Menu r3m1 = new Menu("01", "Cheese Pizza", R.drawable.profile, r3.getId());
+        Menu r3m2 = new Menu("02", "Pattah Spicy Beef Pizza", R.drawable.profile, r3.getId());
+        Menu r3m3 = new Menu("03", "Lankan Pizza", R.drawable.profile, r3.getId());
+        Menu r3m4 = new Menu("04", "Nai Miris Pizza", R.drawable.profile, r3.getId());
+        Menu r3m5 = new Menu("05", "Gindara Cake", R.drawable.profile, r3.getId());
+
         Restaurant r4 = new Restaurant("1237", "Matara Bath kade", R.drawable.matarabathkade);
+        Menu r4m1 = new Menu("01", "Chicken Rice & Curry", R.drawable.profile, r4.getId());
+        Menu r4m2 = new Menu("02", "Beef Rice & Curry", R.drawable.profile, r4.getId());
+        Menu r4m3 = new Menu("03", "Pork Rice & Curry", R.drawable.profile, r4.getId());
+        Menu r4m4 = new Menu("04", "Pol Rotti & Chicken Curry", R.drawable.profile, r4.getId());
+        Menu r4m5 = new Menu("05", "Pol Rotti & Beef Curry", R.drawable.profile, r4.getId());
+
         Restaurant r5 = new Restaurant("1238", "KFC", R.drawable.kfcimage);
-        Menu m1 = new Menu("01", "Fries", R.drawable.profile, r5.getId());
-        Menu m2 = new Menu("02", "Chicken Burger", R.drawable.profile, r5.getId());
-        Menu m3 = new Menu("03", "Beef Burger", R.drawable.profile, r5.getId());
-        Menu m4 = new Menu("04", "Pork Burger", R.drawable.profile, r5.getId());
-        Menu m5 = new Menu("05", "Mutton Burger", R.drawable.profile, r5.getId());
+        Menu r5m1 = new Menu("01", "Fries", R.drawable.profile, r5.getId());
+        Menu r5m2 = new Menu("02", "Chicken Burger", R.drawable.profile, r5.getId());
+        Menu r5m3 = new Menu("03", "Beef Burger", R.drawable.profile, r5.getId());
+        Menu r5m4 = new Menu("04", "Pork Burger", R.drawable.profile, r5.getId());
+        Menu r5m5 = new Menu("05", "Mutton Burger", R.drawable.profile, r5.getId());
 
         rDBm.addRestaurant(r1);
         rDBm.addRestaurant(r2);
@@ -73,10 +93,28 @@ public class MainActivity extends AppCompatActivity {
         rDBm.addRestaurant(r4);
         rDBm.addRestaurant(r5);
 
-        rDBm.addMenu(m1);
-        rDBm.addMenu(m2);
-        rDBm.addMenu(m3);
-        rDBm.addMenu(m4);
-        rDBm.addMenu(m5);
+        rDBm.addMenu(r2m1);
+        rDBm.addMenu(r2m2);
+        rDBm.addMenu(r2m3);
+        rDBm.addMenu(r2m4);
+        rDBm.addMenu(r2m5);
+
+        rDBm.addMenu(r3m1);
+        rDBm.addMenu(r3m2);
+        rDBm.addMenu(r3m3);
+        rDBm.addMenu(r3m4);
+        rDBm.addMenu(r3m5);
+
+        rDBm.addMenu(r4m1);
+        rDBm.addMenu(r4m2);
+        rDBm.addMenu(r4m3);
+        rDBm.addMenu(r4m4);
+        rDBm.addMenu(r4m5);
+
+        rDBm.addMenu(r5m1);
+        rDBm.addMenu(r5m2);
+        rDBm.addMenu(r5m3);
+        rDBm.addMenu(r5m4);
+        rDBm.addMenu(r5m5);
     }
 }
