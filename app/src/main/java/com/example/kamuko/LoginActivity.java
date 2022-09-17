9454df2b6df6efd830bb264a1d4036daa6cd3200
Gavsh96 <78014354+Gavsh96@users.noreply.github.com>
2022-09-17 17:55:32 +0530

    login xml updated

--- app/src/main/java/com/example/kamuko/LoginActivity.java
+++ app/src/main/java/com/example/kamuko/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.example.kamuko;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+
+import com.example.kamuko.databinding.ActivityMainBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        DBModel rDBm = new DBModel();
+        rDBm.load(getApplicationContext());
+
+        FragmentManager frag = getSupportFragmentManager();
+        SignUpFrag signUpFrag = (SignUpFrag) frag.findFragmentById(R.id.LAfragment1);
+        signUpFrag  = new SignUpFrag ();
+        frag.beginTransaction().add(R.id.LAfragment1, signUpFrag ).commit();
+
     }
 }
