cd0af6ae9672bf55a195134b4931b09df2f88f93
Gavsh96 <78014354+Gavsh96@users.noreply.github.com>
2022-09-23 14:23:21 +0530

    OH imp test

--- app/src/main/java/com/example/kamuko/OrderHistoryFragment.java
+++ app/src/main/java/com/example/kamuko/OrderHistoryFragment.java
@@ -3,6 +3,8 @@ package com.example.kamuko;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,10 +13,22 @@ import android.view.ViewGroup;
 
 public class OrderHistoryFragment extends Fragment {
 
+    private DBModel rDBm;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_order_history, container, false);
+        View v = inflater.inflate(R.layout.fragment_order_history, container, false);
+
+        rDBm = new DBModel();
+        rDBm.load(getActivity().getApplicationContext());
+        RecyclerView rv = v.findViewById(R.id.OrderHistoryRecyclerView);
+        rv.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        /*OrderHistoryAdapter adapter = new OrderHistoryAdapter(rDBm, this);
+        rv.setAdapter(adapter);*/
+
+        return v;
     }
 }
